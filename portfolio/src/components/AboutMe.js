a6782174bc5bafcd4b7d1e37f6a62e54735769ad
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2024-10-23 16:08:19 +0000

    Portfolio website update

--- portfolio/src/components/AboutMe.js
+++ portfolio/src/components/AboutMe.js
@@ -6,20 +6,19 @@ export default function AboutMe() {
         <section className="about-me" id="about">
         <div className="about-me-text">
             <span className="about-me-column">
-            <p>
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada. Nullam a quam vel velit pharetra facilisis non nec lorem. Aenean imperdiet, est ut convallis consectetur, felis lorem ultricies felis, non pharetra arcu lorem in est. Integer a est erat. Sed at ipsum ac neque euismod scelerisque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            <p><h3>My Past</h3><br></br>
+            I am a  Full Stack Software Engineer with over five years of experience designing and deploying scalable web applications in cloud environments. With expertise in JavaScript, Node.js, SQL, and AWS, I have developed solutions that have significantly improved system scalability, efficiency, and performance. I has a proven track record in designing cloud-based microservices, optimizing backend systems, and building CI/CD pipelines. I hold a Master’s degree in Computer Information Systems from Boston University and a Bachelor's degree in Information Technology from the University of Massachusetts Boston. My work has consistently enhanced system reliability and user satisfaction.
 </p>
 
             </span>
             <span className="about-me-column">
-            <p>
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada. Nullam a quam vel velit pharetra facilisis non nec lorem. Aenean imperdiet, est ut convallis consectetur, felis lorem ultricies felis, non pharetra arcu lorem in est. Integer a est erat. Sed at ipsum ac neque euismod scelerisque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-</p>
+            <p><h3>My Present</h3><br></br>
+            Mitchell Martinez is currently seeking a new web development role that will allow him to further expand his skillset and contribute to impactful projects. With a strong foundation in full-stack development and cloud-based technologies like AWS and Node.js, Mitchell is looking for opportunities to apply his expertise in building scalable, efficient systems while learning new frameworks and tools. Passionate about continuous growth, he is eager to collaborate on innovative solutions that push the boundaries of web technology, improve user experiences, and solve real-world challenges.</p>
 
             </span>
             <span className="about-me-column">
-            <p>
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada. Nullam a quam vel velit pharetra facilisis non nec lorem. Aenean imperdiet, est ut convallis consectetur, felis lorem ultricies felis, non pharetra arcu lorem in est. Integer a est erat. Sed at ipsum ac neque euismod scelerisque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            <p><h3>My Future</h3><br></br>
+            Looking ahead, Mitchell Martinez aims to find a company where he can make a meaningful impact by leveraging his technical expertise and passion for innovation. His goal is to help scale the company through the development of cutting-edge, scalable web applications that drive business growth and enhance customer satisfaction. Mitchell is committed to staying at the forefront of web technologies, continuously honing his skills, and contributing to a culture of excellence. Ultimately, he aspires to become a key player in a dynamic team, helping the company become an industry lead.
 </p>
 
             </span>
--- portfolio/src/components/MainSection.js
+++ portfolio/src/components/MainSection.js
@@ -6,12 +6,9 @@ const MainSection = () => {
   return (
     <section className="main-section">
       <div className="text">
-        <h1>Hello! My Name is </h1>
-        <br></br>
-        <h2>Mitchell Martinez</h2>
-        <br></br>
-        <h3>Software Developer <span>|</span> Full Stack Engineer</h3>
-        <br></br>
+        <p>Hello! My Name is </p>
+        <p>Mitchell Martinez</p>
+        <p>Software Developer <span>|</span> Full Stack Engineer</p>
         <p>
         Highly motivated recent graduate with a Master's in Computer Information Systems (Web Development) seeking a full-stack developer role. Proven ability to leverage React.js, Java (Spring Boot), and MongoDB to build innovative and user-centric web applications. Experience in technical support and intranet system development.
         </p>
