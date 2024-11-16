93d2ba3eee166699d582d53412e5c6a850380819
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2024-11-16 07:55:52 +0000

    For You/Explore Toggle added with CSS

--- ReactProjects/podcast-app/src/ForYouPage.jsx
+++ ReactProjects/podcast-app/src/ForYouPage.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee, faUser, faComment } from "@fortawesome/free-solid-svg-icons";
 
 const ForYouPage = () => {
+    const [selectedOption, setSelectedOption] = useState("forYou");
+
     return (
         <>
             <style>
@@ -18,6 +21,24 @@ const ForYouPage = () => {
                         color: #007bff;
                         text-align: center;
                     }
+                    .toggle-buttons {
+                        display: flex;
+                        justify-content: center;
+                        margin-bottom: 20px;
+                    }
+                    .toggle-buttons button {
+                        padding: 0;
+                        margin: 0 10px;
+                        border: none;
+                        border-radius: 0px;
+                        cursor: pointer;
+                        color: black;
+                        font-size: 16px;
+                        background-color: transparent;
+                    }
+                    .toggle-buttons button.active {
+                        border-bottom: 2px solid red;
+                    }
                     .for-you-grid {
                         display: grid;
                         grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
@@ -32,10 +53,7 @@ const ForYouPage = () => {
                         flex-direction: column;
                         align-items: center;
                         text-align: center;
-                    }
-
-                    .for-you-card:hover{
-                        background-color: lightyellow;
+                        transition: color 0.3s ease;
                     }
                     .room-name {
                         display: flex;
@@ -54,75 +72,98 @@ const ForYouPage = () => {
                     .for-you-card .icon {
                         margin-right: 5px;
                     }
+
+                    .for-you-card:hover {
+                        background-color: lightyellow;
+                    }
                 `}
             </style>
             <div className="for-you-container">
-                <h2>For you | Explore</h2>
-                <div className="for-you-grid">
-                    <div className="for-you-card">
-                        <div className="room-name">
-                            <FontAwesomeIcon icon={faCoffee} className="icon" />
-                            <h3>Cricket Corner</h3>
-                        </div>
-                        <h4>Tagline for the Podcast</h4>
-                        <p>John Smith, Lisa Lenord</p>
-                        <p>
-                            <FontAwesomeIcon icon={faUser} className="icon" />
-                            100     
-                            <FontAwesomeIcon icon={faComment} className="icon" />
-                            10 
-                        </p>
-                    </div>
-                    <div className="for-you-card">
-                        <div className="room-name">
-                            <FontAwesomeIcon icon={faCoffee} className="icon" />
-                            <h3>Tech Talk</h3>
-                        </div>
-                        <p>Podcast description</p>
-                    </div>
-                    <div className="for-you-card">
-                        <div className="room-name">
-                            <FontAwesomeIcon icon={faCoffee} className="icon" />
-                            <h3>Health & Wellness</h3>
-                        </div>
-                        <p>Podcast description</p>
-                    </div>
-                    <div className="for-you-card">
-                        <div className="room-name">
-                            <FontAwesomeIcon icon={faCoffee} className="icon" />
-                            <h3>Travel Diaries</h3>
-                        </div>
-                        <p>Podcast description</p>
-                    </div>
-                    <div className="for-you-card">
-                        <div className="room-name">
-                            <FontAwesomeIcon icon={faCoffee} className="icon" />
-                            <h3>Travel Diaries</h3>
-                        </div>
-                        <p>Podcast description</p>
-                    </div>
-                    <div className="for-you-card">
-                        <div className="room-name">
-                            <FontAwesomeIcon icon={faCoffee} className="icon" />
-                            <h3>Travel Diaries</h3>
-                        </div>
-                        <p>Podcast description</p>
-                    </div>
-                    <div className="for-you-card">
-                        <div className="room-name">
-                            <FontAwesomeIcon icon={faCoffee} className="icon" />
-                            <h3>Travel Diaries</h3>
-                        </div>
-                        <p>Podcast description</p>
-                    </div>
-                    <div className="for-you-card">
-                        <div className="room-name">
-                            <FontAwesomeIcon icon={faCoffee} className="icon" />
-                            <h3>Travel Diaries</h3>
-                        </div>
-                        <p>Podcast description</p>
-                    </div>
+                <div className="toggle-buttons">
+                    <button
+                        className={selectedOption === "forYou" ? "active" : ""}
+                        onClick={() => setSelectedOption("forYou")}
+                    >
+                        For You
+                    </button>
+                    <button
+                        className={selectedOption === "explore" ? "active" : ""}
+                        onClick={() => setSelectedOption("explore")}
+                    >
+                        Explore
+                    </button>
                 </div>
+                {selectedOption === "forYou" && (
+                    <div className="for-you-grid">
+                        <div className="for-you-card">
+                            <div className="room-name">
+                                <FontAwesomeIcon icon={faCoffee} className="icon" />
+                                <h3>Cricket Corner</h3>
+                            </div>
+                            <h4>Tagline for the Podcast</h4>
+                            <p>John&nbsp;Smith,&nbsp;Lisa&nbsp;Lenord</p>
+                            <p>
+                                <FontAwesomeIcon icon={faUser} className="icon" />
+                                100&nbsp;     
+                                <FontAwesomeIcon icon={faComment} className="icon" />
+                                10 
+                            </p>
+                        </div>
+                        <div className="for-you-card">
+                            <div className="room-name">
+                                <FontAwesomeIcon icon={faCoffee} className="icon" />
+                                <h3>Tech Talk</h3>
+                            </div>
+                            <p>Podcast&nbsp;description</p>
+                        </div>
+                        <div className="for-you-card">
+                            <div className="room-name">
+                                <FontAwesomeIcon icon={faCoffee} className="icon" />
+                                <h3>Health & Wellness</h3>
+                            </div>
+                            <p>Podcast&nbsp;description</p>
+                        </div>
+                        <div className="for-you-card">
+                            <div className="room-name">
+                                <FontAwesomeIcon icon={faCoffee} className="icon" />
+                                <h3>Travel Diaries</h3>
+                            </div>
+                            <p>Podcast&nbsp;description</p>
+                        </div>
+                    </div>
+                )}
+                {selectedOption === "explore" && (
+                    <div className="for-you-grid">
+                        <div className="for-you-card">
+                            <div className="room-name">
+                                <FontAwesomeIcon icon={faCoffee} className="icon" />
+                                <h3>Explore Topic 1</h3>
+                            </div>
+                            <p>Explore&nbsp;description</p>
+                        </div>
+                        <div className="for-you-card">
+                            <div className="room-name">
+                                <FontAwesomeIcon icon={faCoffee} className="icon" />
+                                <h3>Explore Topic 2</h3>
+                            </div>
+                            <p>Explore&nbsp;description</p>
+                        </div>
+                        <div className="for-you-card">
+                            <div className="room-name">
+                                <FontAwesomeIcon icon={faCoffee} className="icon" />
+                                <h3>Explore Topic 3</h3>
+                            </div>
+                            <p>Explore&nbsp;description</p>
+                        </div>
+                        <div className="for-you-card">
+                            <div className="room-name">
+                                <FontAwesomeIcon icon={faCoffee} className="icon" />
+                                <h3>Explore Topic 4</h3>
+                            </div>
+                            <p>Explore&nbsp;description</p>
+                        </div>
+                    </div>
+                )}
             </div>
         </>
     );
